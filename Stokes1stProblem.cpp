f6c4b085fc4225a0143334a1772e14e84e41da7d
JanRotti <jan.rottmayer@web.de>
2021-09-05 14:07:13 +0200

    Ergebnisse, Simulationen, Korrektur

--- Stokes1stProblem.cpp
+++ Stokes1stProblem.cpp
@@ -1,7 +1,7 @@
 //--------------------------------------------------------------------------------------//
 // --------------------------LATTICE BOLTZMANN ALGORITHM--------------------------------//
 //																						//
-// CASE DESCRIPTION: Inkompressible Stokes First Problem 2D 							//
+// CASE DESCRIPTION: Inkompressibles Stokes First Problem 2D 							//
 // AUTHOR: Jan Rottmayer																//
 // INSTITUTION:	TU Kaiserslautern														//
 // DATE: 23.07.2021																		//
@@ -37,7 +37,7 @@ int main()
 	double viscosity = 3e-3;								//kinematic fluid viscosity
 
 	//--------------------------------- FLOW PARAMETERS ------------------------------------//						 
-	double wallVelocity = 3e-3;								//wall velocity at bottom plate
+	double wallVelocity =3e-3;								//wall velocity at bottom plate
 	double maxExpectedVelocity = wallVelocity;				//max. expected velocity
 	
 	//-------------------------------- TIME CONFIGURATION ----------------------------------//
@@ -89,7 +89,7 @@ int main()
 	// 							DATA STRUCTURE INITIALIZATION								//
 	//--------------------------------------------------------------------------------------//
 	
-	// FLUID DENSITY -> first index: x; second index: y
+	//FLUID DENSITY -> first index: x; second index: y
 	double** fluidDensity = new double* [nx]; 
 	for (int k = 0; k < nx; k++)
 	{
@@ -101,8 +101,8 @@ int main()
 		}//end l
 	}//end k
 
-	// FLUID VELOCITY -> first index: x; second index: y;
-	// third index: velocity direction [0->x, 1->y]
+	//FLUID VELOCITY -> first index: x; second index: y;
+	//third index: velocity direction [0->x, 1->y]
 	double*** fluidVelocity = new double** [nx]; 
 	for (int k = 0; k < nx; k++)
 	{
@@ -116,12 +116,12 @@ int main()
 		}//end l
 	}//end k
 
-	// LATTICE WEIGHTS
-	// weigths of lattice directions for equilibrium distribution
+	//LATTICE WEIGHTS
+	//weigths of lattice directions for equilibrium distribution
 	double weigths[9] = { 1./9., 1./9., 1./9., 1./9., 1./36., 1./36., 1./36., 1./36., 4./9.}; 
 
-	// LOCAL DISTRIBUTION -> first index: x; second index: y;
-	// third index: transport(0)/collision(1); fourth index: lattice direction
+	//LOCAL DISTRIBUTION -> first index: x; second index: y;
+	//third index: transport(0)/collision(1); fourth index: lattice direction
 	double**** distribution = new double*** [nx]; 
 	for (int k = 0; k < nx; k++)
 	{
@@ -132,7 +132,7 @@ int main()
 			distribution[k][l][0] = new double[9];
 			distribution[k][l][1] = new double[9];
 			
-			// iterate lattice directions
+			//iterate lattice directions
 			for (int i = 0; i < 9; i++) 
 			{
 				// initialize with rho and u=0;
@@ -142,8 +142,8 @@ int main()
 		}//end l
 	}//end k
 
-	// LATTICE VELOCITIES -> first array index: lattice direction; 
-	// second index velocity direction (0:x-dir;1:y-dir)
+	//LATTICE VELOCITIES -> first array index: lattice direction; 
+	//second index velocity direction (0:x-dir;1:y-dir)
 	double xsi[9][2];
 	xsi[0][0] = xsi0; xsi[1][0] = 0; xsi[2][0] = -xsi0; xsi[3][0] = 0; xsi[4][0] = xsi0;
 	xsi[5][0] = -xsi0; xsi[6][0] = -xsi0; xsi[7][0] = xsi0; xsi[8][0] = 0;
@@ -204,12 +204,12 @@ int main()
 	//--------------------------------------------------------------------------------------//
 	
 	remove("history.txt");
-	// error history
+	//error history
 	ofstream errorFile;
-	// open error file
+	//open error file
 	errorFile.open("history.txt");
 	
-	// start time
+	//start time
 	long t = 0;
 
 	//------------------------------------ TIME LOOP ---------------------------------------//
@@ -218,17 +218,17 @@ int main()
 	
 		//---------------------------------- TRANSPORT STEP --------------------------------//
 		#pragma omp parallel for
-		// iterate x-direction
+		//iterate x-direction
 		for (int k = 1; k < nx - 1; ++k) 
 		{	
-			// iterate y-direction
+			//iterate y-direction
 			for (int l = 1; l < ny - 1; ++l) 
 			{
-				// iterate lattice directions
+				//iterate lattice directions
 				for (int i = 0; i < 9; ++i) { 
-					// initialize neighbor coordinates
+					//initialize neighbor coordinates
 					int kNN = 0, lNN = 0;
-					// directional push coordinates
+					//directional push coordinates
 					switch (i) {
 					case 0:
 					{
@@ -284,8 +284,8 @@ int main()
 						lNN = l;
 						break;
 					}
-					} //end switch
-					// push transport of distributions
+					}//end switch
+					//push transport of distributions
 					distribution[kNN][lNN][0][i] = distribution[k][l][1][i]; 
 				}//end i
 			}//end l
@@ -293,17 +293,17 @@ int main()
 
 
 		//-------------------------------- PERIODIC BOUNDARY -------------------------------//
-		// right boundary transport
+		//right boundary transport
 		#pragma omp parallel for
-		// iterate y-direction
+		//iterate y-direction
 		for (int l = 1; l < ny - 1; ++l) { 
-			// iterate lattice directions
+			//iterate lattice directions
 			for (int i = 0; i < 9; ++i) { 
-				// initialize neighbor coordinates
+				//initialize neighbor coordinates
 				int kNN = 0, lNN = 0;
-				// fix x to right boundary
+				//fix x to right boundary
 				int k = nx - 1;
-				// directional push coordinates
+				//directional push coordinates
 				switch (i) {
 				case 0:
 				{
@@ -359,23 +359,23 @@ int main()
 					lNN = l;
 					break;
 				}
-				} //end switch
-				// push transport of right boundary distribution
+				}//end switch
+				//push transport of right boundary distribution
 				distribution[kNN][lNN][0][i] = distribution[k][l][1][i]; 
 			}//end i
 		}//end l
 
-		// left boundary transport
+		//left boundary transport
 		#pragma omp parallel for
-		// iterate y-direction
+		//iterate y-direction
 		for (int l = 1; l < ny - 1; l++) {
-			// iterate lattice directions
+			//iterate lattice directions
 			for (int i = 0; i < 9; ++i) { 
-				// initialize neighbor coordinates
+				//initialize neighbor coordinates
 				int kNN = 0, lNN = 0;
-				// fix x to left boundary
+				//fix x to left boundary
 				int k = 0;
-				// directional push coordinates
+				//directional push coordinates
 				switch (i) {
 				case 0:
 				{
@@ -432,34 +432,34 @@ int main()
 					break;
 				}
 				}//end switch
-				// push transport of left boundary distribution
+				//push transport of left boundary distribution
 				distribution[kNN][lNN][0][i] = distribution[k][l][1][i]; 
 			}//end i
-		} //end l
+		}//end l
 
 
 		//------------------------ BOUNCE BACK WITH WALL VELOCITY --------------------------//
-		// bottom wall --> half way bounce back with fixed wall velocity
+		//bottom wall --> half way bounce back with fixed wall velocity
 		#pragma omp parallel for
 		// iterate x-direction
 		for (int k = 1; k < nx - 1; ++k) {
-			// fix y to bottom boundary
+			//fix y to bottom boundary
 			int l = 0; 
-			// fixed wall velocity factor
+			//fixed wall velocity factor
 			double velocityTerm = 2  * wallVelocity / pow(speedOfSound, 2);
-			// bounce back of wall distributions --> negative y-direction
+			//bounce back of wall distributions --> negative y-direction
 			distribution[k][l + 1][0][1]	 = distribution[k][l][0][3] - fluidDensity[k][l + 1] * weigths[3] * xsi[3][0] * velocityTerm;		//i = 1	
 			distribution[k + 1][l + 1][0][4] = distribution[k][l][0][6] - fluidDensity[k + 1][l + 1] * weigths[6] * xsi[6][0] * velocityTerm;	//i = 4
 			distribution[k - 1][l + 1][0][5] = distribution[k][l][0][7] - fluidDensity[k -1][l + 1] * weigths[7] * xsi[7][0] * velocityTerm;	//i = 5
 		}
 
-		// bounce back corner node(0,0)
+		//bounce back corner node(0,0)
 		distribution[0][1][0][1]	  = distribution[0][0][0][3] - 2 * fluidDensity[0][1] * weigths[3] * xsi[3][0] * wallVelocity / pow(speedOfSound, 2);	  //i = 1	
 		distribution[1][1][0][4]	  = distribution[0][0][0][6] - 2 * fluidDensity[1][1] * weigths[6] * xsi[6][0] * wallVelocity / pow(speedOfSound, 2);	  //i = 4
 		distribution[nx - 1][1][0][5] = distribution[0][0][0][7] - 2 * fluidDensity[nx - 1][1] * weigths[7] * xsi[7][0] * wallVelocity / pow(speedOfSound, 2);//i = 5
 		
 		
-		// bounce back corner node(nx-1,0)
+		//bounce back corner node(nx-1,0)
 		distribution[nx - 1][1][0][1] = distribution[nx - 1][0][0][3] - 2 * fluidDensity[nx - 1][1] * weigths[3] * xsi[3][0] * wallVelocity / pow(speedOfSound, 2);//i = 1
 		distribution[0][1][0][4]	  = distribution[nx - 1][0][0][6] - 2 * fluidDensity[0][1] * weigths[6] * xsi[6][0] * wallVelocity / pow(speedOfSound, 2);	   //i = 4
 		distribution[nx - 2][1][0][5] = distribution[nx - 1][0][0][7] - 2 * fluidDensity[nx - 2][1] * weigths[7] * xsi[7][0] * wallVelocity / pow(speedOfSound, 2);//i = 5
@@ -468,47 +468,37 @@ int main()
 		//------------------------- SPECULAR REFLECTION BOUNDARY ---------------------------//
 		//top wall --> specular reflection
 		#pragma omp parallel for
-		// iterate x-direction
-		for (int k = 1; k < nx - 1; ++k) {
-			// fix y to top boundary
+		//iterate x-direction
+		for (int k = 0; k <= nx - 1; ++k) {
+			//fix y to top boundary
 			int l = ny - 1; 
-			// specular reflection of wall distributions --> positive y-direction
-			distribution[k][l - 1][0][3]	 = distribution[k][l][0][1];  //i = 3	
-			distribution[k - 1][l - 1][0][6] = distribution[k][l][0][5];  //i = 6
-			distribution[k + 1][l - 1][0][7] = distribution[k][l][0][4];  //i = 7
+			//specular reflection of wall distributions --> positive y-direction
+			distribution[k][l - 1][0][3] = distribution[k][l][0][1];  //i = 3	
+			distribution[k][l - 1][0][6] = distribution[k][l][0][5];  //i = 6
+			distribution[k][l - 1][0][7] = distribution[k][l][0][4];  //i = 7
 		}
-
-		// specular reflection corner node(0,ny-1)
-		distribution[0][ny - 2][0][3]	   = distribution[0][ny - 1][0][1]; //i = 3
-		distribution[nx - 1][ny - 2][0][6] = distribution[0][ny - 1][0][5]; //i = 6
-		distribution[1][ny - 2][0][7]	   = distribution[0][ny - 1][0][4]; //i = 7
-		
-		// specular reflection corner node(nx-1,ny-1)
-		distribution[nx - 1][ny - 2][0][3] = distribution[nx - 1][ny - 1][0][1]; //i = 3
-		distribution[nx - 2][ny - 2][0][6] = distribution[nx - 1][ny - 1][0][5]; //i = 6
-		distribution[0][ny - 2][0][7]	   = distribution[nx - 1][ny - 1][0][4]; //i = 7
 		
 
 		//---------------------------- COMPUTATION OF MOMENTS ------------------------------//
 		#pragma omp parallel for
-		// iterate x-direction
+		//iterate x-direction
 		for (int k = 0; k <= nx - 1; k++) {
-			// iterate y-direction
+			//iterate y-direction
 			for (int l = 1; l < ny - 1; l++) {
-				// resetting moments
+				//resetting moments
 				fluidDensity[k][l] = 0;
 				fluidVelocity[k][l][0] = 0;
 				fluidVelocity[k][l][1] = 0;
-				// iterate lattice direction
+				//iterate lattice direction
 				for (int i = 0; i < 9; i++) { 
-					// density
+					//density
 					fluidDensity[k][l] += distribution[k][l][0][i]; 
-					// fluid momentum in x and y direction
+					//fluid momentum in x and y direction
 					fluidVelocity[k][l][0] += xsi[i][0] * distribution[k][l][0][i];
 					fluidVelocity[k][l][1] += xsi[i][1] * distribution[k][l][0][i]; 
 				}//end i
 
-				// fluid velocity from momentum
+				//fluid velocity from momentum
 				fluidVelocity[k][l][0] = fluidVelocity[k][l][0] / fluidDensity[k][l];
 				fluidVelocity[k][l][1] = fluidVelocity[k][l][1] / fluidDensity[k][l];
 			}//end l
@@ -517,18 +507,18 @@ int main()
 
 		//-------------------------------- COLLISION STEP ----------------------------------//
 		#pragma omp parallel for
-		// iterate x-direction
+		//iterate x-direction
 		for (int k = 0; k <= nx - 1; k++) {
-			// iterate y-direction
+			//iterate y-direction
 			for (int l = 1; l < ny - 1; l++) { 
-				// iterate lattice direction
+				//iterate lattice direction
 				for (int i = 0; i < 9; i++) { 
-					// compute temporary skalar product terms
+					//compute temporary skalar product terms
 					double dotProductXsiVelo = xsi[i][0] * fluidVelocity[k][l][0] + xsi[i][1] * fluidVelocity[k][l][1];
 					double dotProductVelo = pow(fluidVelocity[k][l][0], 2) + pow(fluidVelocity[k][l][1], 2);
-					// compute equilibrium distribution
+					//compute equilibrium distribution
 					double eqDistribution = weigths[i] * fluidDensity[k][l] * (1 + dotProductXsiVelo / pow(speedOfSound, 2) + pow(dotProductXsiVelo, 2) / (2 * pow(speedOfSound, 4)) - dotProductVelo / (2 * pow(speedOfSound, 2)));
-					// update distribution with Bhatnagar-Gross-Krook model 
+					//update distribution with Bhatnagar-Gross-Krook model 
 					distribution[k][l][1][i] = distribution[k][l][0][i] + omega * (eqDistribution - distribution[k][l][0][i]); 
 				}//end i
 			}//end l
@@ -536,29 +526,29 @@ int main()
 
 
 		//------------------------------------ OUTPUT --------------------------------------//
-		// file output 
+		//file output 
 		if (t % writeInterval == 0) {
 			writeResults(t, deltaT, nx, ny, deltaX, fluidDensity, speedOfSound, fluidVelocity);
 		}
-		// progress bar and analytical error
+		//progress bar and analytical error
 		float progress = t / float(timeSteps);
 
 		//--------------------------- CONVERGENCE CRITERIA -----------------------------//
-		// deviation to analytical solution
+		//deviation to analytical solution
 		int k = nx / 2;
 		double analyticalSolution = 0;
-		// compute y coordinates
+		//compute y coordinates
 		double error = 0;
 		for(int l = 1; l < ny - 1;l++){
 			error += abs(fluidVelocity[k][l][0] - (wallVelocity - wallVelocity * erf(deltaX * (float(l) - 0.5) / (2 * sqrt(viscosity * (t * deltaT))))));
 		}
-		// normalize error
-		error /= (ny - 2);
+		//normalize error
+		//error /= (ny - 2);
 
-		// write to error file
+		//write to error file
 		errorFile << (t * deltaT) << ", " << error << endl;
 		
-		// update on every progress percent
+		//progress bar with analytical error
 		if (t %  100 == 0) {
 			int barWidth = 40;
 			cout << "[";
@@ -579,9 +569,8 @@ int main()
 			cout.flush();
 		}//end if
 	}//end time loop 
-	// close history file
+	//close history file
 	errorFile.close();
-	
 
 	//write final result
 	writeResults(t, deltaT, nx, ny, deltaX, fluidDensity, speedOfSound, fluidVelocity);
